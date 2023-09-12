4fd1b1a81e4747acbd2d6f871c876de9357a014d
Afaq Ahmad <ufaq302@yahoo.com>
2023-09-13 00:08:12 +0500

    service_worker added

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,14 +1,44 @@
+const staticCacheName = 'static-site'
+const assets = [
+    '/',
+    '/app.js',
+    '/favicon.icon',
+    '/index.html',
+    '/sw.js',
+    '/manifest.json',
+    '/static/js/main.fcf05910.js',
+    '/static/css/main.8e48ab8f.css',
+    '/static/js/bundle.js',
+    '/icon-192x192.png',
+    '/icon-256x256.png',
+    '/icon-384x384.png',
+    '/icon-512x512.png'
+]
+
+
 //Install service worker
 self.addEventListener('install', event => {
-    console.log('Service worker installed!')
+    // console.log('Service worker installed!')s
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('Caching shell assets');
+            cache.addAll(assets)
+        })
+    )
 });
 
 //Activate event
 self.addEventListener("activate", event => {
-    console.log("Service worker has been activated!")
+    // console.log("Service worker has been activated!")
 })
 
-//Fetch event
+//Fetch events
 self.addEventListener("fetch", event => {
-    console.log("Fetch event", event);
+    // console.log("Fetch event", event);
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return cacheRes || fetch(event.request)
+        })
+    )
+
 })
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from './Components/About';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 
+
+const router = createHashRouter([{
+  path: "/",
+  element: <App />,
+},
+{
+  path: "/about",
+  element: <About />
+}
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
